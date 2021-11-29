b39e90423ea15f1da6e3daed98afa35d987064df
Graham Koenig <89868570+Koeniggw@users.noreply.github.com>
2021-11-29 02:59:10 +0000

    added javadoc comments

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 //@author Graham Koenig, Tanner Daria, Alex Hils, Maggie Korte
-//@version 1.0
+//@version 2.0
 //@since  11/30/2021
 //This project creates a Jeopardy like game where the user can select 6 different questions each with 4 multiple choice options. Different questions are worth different point values. 
 
